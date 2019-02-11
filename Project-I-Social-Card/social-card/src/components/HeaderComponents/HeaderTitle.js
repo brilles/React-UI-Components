ca47d5ee89f430f372d8fd668647a5fe9b014f61
Brian Illes <brianrilles@gmail.com>
2019-02-11 13:04:53 -0800

    added unstyled content

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return <div className="title">Lambda School @LambdaSchool • 26 jan</div>;
+};
+
+export default HeaderTitle;
