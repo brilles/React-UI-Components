c2880f0b87a932756fd60d933551a2f1f6438943
Brian Illes <brianrilles@gmail.com>
2019-02-12 15:32:55 -0800

    stretch, add class with state

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -5,46 +5,54 @@ import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay"
 import NumberButton from "./components/ButtonComponents/NumberButton";
 import ActionButton from "./components/ButtonComponents/ActionButton";
 
-const App = () => {
-  return (
-    <div className="calculator-container">
-      <CalculatorDisplay number="0" />
-      <div className="input">
-        <div className="grey-col">
-          <div className="row-1">
-            <ActionButton text="clear" />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: [7, 8, 9, 4, 5, 6, 1, 2, 3]
+    };
+  }
+  render() {
+    return (
+      <div className="calculator-container">
+        <CalculatorDisplay number="0" />
+        <div className="input">
+          <div className="grey-col">
+            <div className="row-1">
+              <ActionButton text="clear" />
+            </div>
+            <div className="row-2">
+              <NumberButton text={this.state.text[0]} />
+              <NumberButton text={this.state.text[1]} />
+              <NumberButton text={this.state.text[2]} />
+            </div>
+            <div className="row-3">
+              <NumberButton text={this.state.text[3]} />
+              <NumberButton text={this.state.text[4]} />
+              <NumberButton text={this.state.text[5]} />
+            </div>
+            <div className="row-4">
+              <NumberButton text={this.state.text[6]} />
+              <NumberButton text={this.state.text[7]} />
+              <NumberButton text={this.state.text[8]} />
+            </div>
+            <div className="row-5">
+              <ActionButton text="0" />
+            </div>
           </div>
-          <div className="row-2">
-            <NumberButton text="7" />
-            <NumberButton text="8" />
-            <NumberButton text="9" />
-          </div>
-          <div className="row-3">
-            <NumberButton text="4" />
-            <NumberButton text="5" />
-            <NumberButton text="6" />
-          </div>
-          <div className="row-4">
-            <NumberButton text="1" />
-            <NumberButton text="2" />
-            <NumberButton text="3" />
-          </div>
-          <div className="row-5">
-            <ActionButton text="0" />
-          </div>
-        </div>
-        <div className="red-col">
-          <div className="operators">
-            <ActionButton buttonStyle="red" text="÷" />
-            <ActionButton buttonStyle="red" text="x" />
-            <ActionButton buttonStyle="red" text="−" />
-            <ActionButton buttonStyle="red" text="+" />
-            <ActionButton buttonStyle="red" text="=" />
+          <div className="red-col">
+            <div className="operators">
+              <ActionButton buttonStyle="red" text="÷" />
+              <ActionButton buttonStyle="red" text="x" />
+              <ActionButton buttonStyle="red" text="−" />
+              <ActionButton buttonStyle="red" text="+" />
+              <ActionButton buttonStyle="red" text="=" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
